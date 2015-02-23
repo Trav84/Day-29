efa673e6c9c5169f28bae06cfd0903a5d5ebabe2
Travis Czerw <travis.czerw@gmail.com>
2015-02-23 17:22:56 -0600

    Got filter to work for both name and abb.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,7 +4,10 @@ angular.module('app.controllers', []).controller('sortController', function($sco
 	$scope.stateList = [];
 	$scope.changeArray = [];
 	$scope.abb = true;
+	$scope.alpha = true;
 	$scope.filterBy = '';
+	$scope.stateArrow = true;
+	$scope.abbArrow = true;
 
 	function getRequest() {
 
@@ -29,7 +32,17 @@ angular.module('app.controllers', []).controller('sortController', function($sco
 	getRequest();
 
 	$scope.stateClick = function() {
-		$scope.changeArray.reverse();
+		if($scope.alpha) {
+			$scope.changeArray = _.sortBy($scope.stateList, function(element) {
+				$scope.alpha = true;
+				return element.name + element.abbreviation;
+			});
+		} else {
+			$scope.changeArray.reverse();
+			$scope.alpha = false;
+		}
+		$scope.stateArrow = !$scope.stateArrow;
+
 	};
 
 	$scope.abbClick = function() {
@@ -42,15 +55,12 @@ angular.module('app.controllers', []).controller('sortController', function($sco
 			$scope.changeArray.reverse();
 			$scope.abb = true;
 		}
+		$scope.abbArrow = !$scope.abbArrow;
 	};
 
 	$scope.$watch('filterBy', function() {
-		$scope.changeArray.name
 		$scope.changeArray = _.filter($scope.stateList, function(element) {
-			return element.name.indexOf($scope.filterBy) >= 0;
+			return element.name.indexOf($scope.filterBy) >= 0 ||  element.abbreviation.indexOf($scope.filterBy) >= 0;
 		});
-		// $scope.changeArray = _.filter($scope.stateList, function(element) {
-		// 	return element.abbreviation.indexOf($scope.filterBy) >= 0;
-		// });
 	});
 });
